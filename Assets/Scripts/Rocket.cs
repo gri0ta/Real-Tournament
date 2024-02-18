89aaf17abecd0a1d74cde9e04fab3a8a75b8b491
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-02-18 14:00:04 +0200

    Shooting effects

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 	public float speed = 20;
+	public GameObject explosionPrefab;
 
 	void Start()
 	{
@@ -24,5 +25,6 @@ public class Rocket : MonoBehaviour
         {
 			health.Damage(10);
         }
+		Instantiate(explosionPrefab, transform.position, transform.rotation);
 	}
 }
