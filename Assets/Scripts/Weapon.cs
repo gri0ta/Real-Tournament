2d75e3e04357aa7a136cf23f8832cd4274e8b62e
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 11:25:12 +0200

    Reload, AutoFire

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,18 +6,62 @@ public class Weapon : MonoBehaviour
 {
 	public GameObject bulletPrefab;
 
+	public int maxAmmo;
+	public int ammo;
+	public bool isReloading;
+	public bool isAutoFire;
+	public float shootInterval;
+	float waitTimeLeft;
+
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Mouse0))
+		if (!isAutoFire && Input.GetKeyDown(KeyCode.Mouse0))
 		{
 			Shoot();
 		}
+
+		if (isAutoFire && Input.GetKey(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			Reload();
+		}
+
+		waitTimeLeft -= Time.deltaTime;
 	}
 
+	async void Reload()
+	{
+		if (isReloading) return;
+		isReloading = true;
+
+		await new WaitForSeconds(2f);
+
+		ammo = maxAmmo;
+		isReloading = false;
+	}
 
 	void Shoot()
 	{
+		if (isReloading) return;
+
+		if (ammo <= 0)
+		{
+			Reload();
+			return;
+		}
+
+		if (waitTimeLeft > 0) return;
+
+
+		ammo--;
+		waitTimeLeft = shootInterval;
+
 		var cam = Camera.main.transform;
 		Instantiate(bulletPrefab,cam.position,cam.rotation);
+
 	}
 }
